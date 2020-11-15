1ec21c038a37bac433d13fefe78df291acf65c60
lekegitrepo <lekedevrepo@gmail.com>
2020-11-15 08:54:59 -0600

    add linter configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # add the other user registration info columns later
 end
