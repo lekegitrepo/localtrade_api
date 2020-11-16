0d51971ee683fea0b66598c4eee56d291352d8fe
lekegitrepo <lekedevrepo@gmail.com>
2020-11-16 12:14:28 -0600

    add test for the user's credentials

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -5,5 +5,19 @@ RSpec.describe Api::V1::SessionsController do
     before(:each) do
       @user = FactoryBot.create :user
     end
+
+    context 'When user credentials are correct' do
+      before(:each) do
+        credentials = { email: @user.email, password: '123456789' }
+        post :create, params: { session: credentials }
+      end
+
+      it 'returns the user record corresponding to the given credentials' do
+        @user.reload
+        expect(json_response[:authentication_token]).to eql @user.authentication_token
+      end
+
+      it { should respond_with 200 }
+    end
   end
 end
