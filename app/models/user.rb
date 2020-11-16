59529995680931b156f3d2f7123f371445400c3d
lekegitrepo <lekedevrepo@gmail.com>
2020-11-16 04:50:49 -0600

    create generate_new_auth_token method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # add the other user registration info columns later
+  def generate_new_auth_token
+    token = User.generate_unique_secure_token
+
+    update_attributes authentication_token: token
+  end
 end
