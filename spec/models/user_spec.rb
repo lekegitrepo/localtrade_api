60093e98cc3b9f8b5df73504dfdba02737532760
lekegitrepo <lekedevrepo@gmail.com>
2020-11-16 11:51:55 -0600

    add test for the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryBot.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authentication_token) }
+
+  it { should validate_uniqueness_of(:authentication_token) }
+
+  it { should be_valid }
 end
