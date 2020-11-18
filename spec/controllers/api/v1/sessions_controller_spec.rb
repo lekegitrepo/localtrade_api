f0c4a6cceb05a1bcb4f13df0fefd9179544e9f24
lekegitrepo <lekedevrepo@gmail.com>
2020-11-18 12:54:09 -0600

    fix the test error

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::SessionsController do
+RSpec.describe Api::V1::SessionsController, type: :controller do
   describe 'POST #create' do
     before(:each) do
       @user = FactoryBot.create :user
@@ -14,8 +14,16 @@ RSpec.describe Api::V1::SessionsController do
 
       it 'returns the user record corresponding to the given credentials' do
         @user.reload
-        expect(json_response[:authentication_token]).to eql @user.authentication_token
+        json = JSON.parse(response.body)
+        p "This is user token: #{@user.authentication_token} json[:authentication_token]: #{json['id']}"
+        expect(json['authentication_token']).to eql @user.authentication_token
       end
+
+      # it 'returns the user record corresponding to the given credentials' do
+      #   @user.reload
+      #   p "This is user in the test: #{@user.authentication_token}"
+      #   expect(json_response[:authentication_token]).to eql @user.authentication_token
+      # end
 
       it { should respond_with 200 }
     end
