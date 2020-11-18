4e49850c0838e2c529ee421128ecc859608b7bf5
lekegitrepo <lekedevrepo@gmail.com>
2020-11-18 13:06:23 -0600

    remove unused import statements

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,8 +6,6 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'devise'
-require_relative 'support/controller_macros'
 require 'support/request_helpers'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -69,9 +67,6 @@ RSpec.configure do |config|
 
   # Add these
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include FactoryBot::Syntax::Methods
-  config.extend ControllerMacros, type: :controller
-
 end
 
 Shoulda::Matchers.configure do |config|
