e5f207a4921fb27766433671ef743c1eee7ea892
lekegitrepo <lekedevrepo@gmail.com>
2020-11-19 15:00:05 -0600

    update the return json value of the render_json method

--- app/controllers/concerns/json_response.rb
+++ app/controllers/concerns/json_response.rb
@@ -1,11 +1,9 @@
 module JsonResponse
   def render_json(user, msg, success, status, loc)
-    # render json: {
-    #   messages: message,
-    #   is_success: is_success,
-    #   user: user
-    # }, status: status, location: loc
-
-    render json: user, message: msg, is_success: success, status: status, location: loc
+    render json: {
+      user: user,
+      message: msg,
+      is_success: success
+    }, status: status, location: loc
   end
 end
