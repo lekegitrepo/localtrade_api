d7a9bbf8b04dc5a1593aa3d4379dce1b5c7fd292
lekegitrepo <lekedevrepo@gmail.com>
2020-11-27 13:22:32 -0600

    add create route for the user resources route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show]
+      resources :users, only: [:show, :create]
       resources :sessions, only: [:create, :destroy]
     end
   end
