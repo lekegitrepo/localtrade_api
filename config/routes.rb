6b955f37405110cd5c86a83c2fda9f4edd89eb26
lekegitrepo <lekedevrepo@gmail.com>
2020-11-27 09:27:55 -0600

    update the route to specify user show route only

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users
+      resources :users, only: [:show]
       resources :sessions, only: [:create, :destroy]
     end
   end
