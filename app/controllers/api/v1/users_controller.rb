66cfb6950072bcf6b49e12f430b10d3037013419
lekegitrepo <lekedevrepo@gmail.com>
2020-11-27 13:34:22 -0600

    create user_params method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,4 +14,10 @@ class Api::V1::UsersController < ApplicationController
     user = User.find_by(id: params[:id])
     render_json 'Profile', true, user, :ok # , [:api_v1, user]
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
