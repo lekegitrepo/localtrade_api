c26182c0a8fe10c7e1175920ec8625936773a9d5
lekegitrepo <lekedevrepo@gmail.com>
2020-11-29 10:39:49 -0600

    add destroy route to the users resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show, :create, :update]
+      resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
     end
   end
