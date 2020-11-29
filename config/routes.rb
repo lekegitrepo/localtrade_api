f754b4df471812e2c36e86c9e53e12662c806f7d
lekegitrepo <lekedevrepo@gmail.com>
2020-11-29 10:00:07 -0600

    add update route to the users resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show, :create]
+      resources :users, only: [:show, :create, :update]
       resources :sessions, only: [:create, :destroy]
     end
   end
