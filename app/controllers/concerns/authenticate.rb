748b3fbe642f4d66016cfd7b908a1b68a605f91d
lekegitrepo <lekedevrepo@gmail.com>
2020-11-30 03:02:00 -0600

    create authenticate_with_token method

--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -5,4 +5,10 @@ module Authenticate
   def current_user
     @current_user ||= User.find_by(authentication_token: request.headers['Authorization'])
   end
+
+  def authenticate_with_token
+    unless user_signed_in?
+      render_json 'Not Authenticated', false, { errors: 'Unauthorized' }, :unauthorized
+    end
+  end
 end
