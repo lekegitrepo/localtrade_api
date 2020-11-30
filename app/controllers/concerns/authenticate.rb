55f0db3090183990523c8a55a550202ac7555341
lekegitrepo <lekedevrepo@gmail.com>
2020-11-30 03:03:05 -0600

    create user_signed_in method

--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -6,6 +6,10 @@ module Authenticate
     @current_user ||= User.find_by(authentication_token: request.headers['Authorization'])
   end
 
+  def user_signed_in?
+    current_user.present?
+  end
+
   def authenticate_with_token
     unless user_signed_in?
       render_json 'Not Authenticated', false, { errors: 'Unauthorized' }, :unauthorized
