4616e5667e8dd39c33a87cd1055ee280a8b894de
lekegitrepo <lekedevrepo@gmail.com>
2020-12-01 03:11:39 -0600

    rename the session cookie store key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authen_app', domain: ''
+  Rails.application.config.session_store :cookie_store, key: '_local_trade', domain: ''
 else
-  Rails.application.config.session_store :cookie_store, key: '_authen_app'
+  Rails.application.config.session_store :cookie_store, key: '_local_trade'
 end
