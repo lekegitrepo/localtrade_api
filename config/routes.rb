79d928fd08ed7e1f7eb3d28b623df2c82ad1858e
lekegitrepo <lekedevrepo@gmail.com>
2020-12-01 05:06:46 -0600

    update and customise user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
+  # namespace :api, defaults: { format: :json } do
+  #   namespace :v1 do
+  #     resources :users, only: [:show, :create, :update, :destroy]
+  #     resources :sessions, only: [:create, :destroy]
+  #   end
+  # end
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show, :create, :update, :destroy]
+      post 'sign_up', to: 'users#create'
+      # get 'user_profile', to: 'users#show'
+      post 'sign_in', to: 'sessions#create'
+      delete 'logged_out', to: 'sessions#destroy'
+      # post 'sign_up', to: 'users#update'
+      # patch 'sign_up', to: 'users#update'
+      post 'sign_up', to: 'users#create'
+      resources :users, only: [:show, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
     end
   end
