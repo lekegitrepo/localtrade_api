f4d377eca8cb97f0b88484dd268a415b313895ba
lekegitrepo <lekedevrepo@gmail.com>
2020-12-01 08:57:58 -0600

    add domain address to the session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_local_trade', domain: ''
+  Rails.application.config.session_store :cookie_store, key: '_local_trade', domain: 'https://localtradeapi.herokuapp.com/'
 else
   Rails.application.config.session_store :cookie_store, key: '_local_trade'
 end
