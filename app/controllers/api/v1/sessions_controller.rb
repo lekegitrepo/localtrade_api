3a3ca1a66b93704ec85665fe1673d0bebec475e1
lekegitrepo <lekedevrepo@gmail.com>
2020-12-12 08:24:22 -0600

    comment out the new response.header

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::SessionsController < ApplicationController
         same_site: 'None'
       }
       response.headers['Set-Cookie'] = 'Secure;SameSite=None;Secure=true'
-      response.headers['Set-Cookie'] = { same_site: 'None', secure: true, value: user.id, name: '_session'}
+      # response.headers['Set-Cookie'] = { same_site: 'None', secure: true, value: user.id, name: '_session' }
       render_json 'Signed in', true, user, :ok, [:api_v1, user]
     else
       render_json 'Unable to Signed in: wrong email or password', false, {}, :unprocessable_entity
