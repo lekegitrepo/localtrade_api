75e34f8c270a6a476df3578da9b4e9c2c828a7aa
lekegitrepo <lekedevrepo@gmail.com>
2021-01-23 02:38:47 -0600

    update the json_response method

--- app/controllers/concerns/json_response.rb
+++ app/controllers/concerns/json_response.rb
@@ -1,5 +1,5 @@
 module JsonResponse
-  def render_json(msg, success, user,  status, loc = nil)
+  def render_json(msg, success, user, status, loc = nil)
     render json: {
       user: user,
       message: msg,
