ff1c097c824c79dc5b8f3f95189ce3522c5787e5
leke <lekedevrepo@gmail.com>
2021-01-28 13:31:44 -0500

    remove the migration file for the index fix

--- db/migrate/20210125183445_remove_index_from_authentication_token.rb
+++
@@ -1,9 +0,0 @@
-class RemoveIndexFromAuthenticationToken < ActiveRecord::Migration[6.0]
-  def up
-    remove_index :users, :authentication_token
-  end
-
-  def down
-    add_index :users, :authentication_token
-  end
-end
